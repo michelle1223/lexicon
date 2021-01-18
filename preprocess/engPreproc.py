92912c3e9da50b630ff35c0ed5fa7f56213e21a8
Michelle Min <59953188+michelle1223@users.noreply.github.com>
2021-01-19 00:14:08 +0900

    Create engPreproc.py

---
+++ preprocess/engPreproc.py
@@ -0,0 +1,42 @@
+import pandas as pd
+import re
+import nltk
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+from nltk.stem import PorterStemmer
+from nltk.stem import LancasterStemmer
+from nltk.corpus import stopwords 
+stop_words = set(stopwords.words('english')) 
+nltk.download('punkt')
+nltk.download('wordnet')
+nltk.download('stopwords')
+
+def cleanText(readData):
+    #텍스트에 포함되어 있는 특수 문자 제거 (단, $, % 제외)
+    text = re.sub('[-=+,#/\?:^.@*\"※~&ㆍ!』\\‘|\(\)\[\]\<\>`\'…》]', '', str(readData))
+    #길이 1~2인 단어 제거 (숫자/특수문자 제외)
+    shortword = re.compile(r'\W*\b[a-zA-Z]{1,2}\b')
+    cleaned_text = shortword.sub('', text)
+    return cleaned_text
+
+def listToString(l):  
+    # initialize an empty string 
+    str1 = " " 
+    # return string   
+    return (str1.join(l))
+    
+# 초기 설정: stopwords, lemmatizer, stemmer 설정 필요!
+n = WordNetLemmatizer()
+s = PorterStemmer()
+
+def textPreproc(text):
+    temp = cleanText(text)
+    words = word_tokenize(temp)
+    result = []
+    for w in words: 
+        if w not in stop_words: 
+            result.append(w) 
+    lresult = [n.lemmatize(w) for w in result]
+    sresult = [s.stem(w) for w in lresult]
+    final = listToString(sresult)
+    return final
